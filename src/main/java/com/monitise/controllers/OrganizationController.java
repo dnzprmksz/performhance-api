58cc5b76d981985d8ea9f5e88f1695e9a20b95fb
Deniz Parmaksız <denizparmaksiz@gmail.com>
2016-08-08 13:52:55 +0300

    Added log support and logged incoming request in controllers, closing #13.

--- src/main/java/com/monitise/controllers/OrganizationController.java
+++ src/main/java/com/monitise/controllers/OrganizationController.java
@@ -8,6 +8,8 @@ import com.monitise.models.Role;
 import com.monitise.models.User;
 import com.monitise.services.OrganizationService;
 import com.monitise.services.UserService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,9 +28,12 @@ public class OrganizationController {
     private OrganizationService organizationService;
     @Autowired
     private UserService userService;
+    private static final Logger LOGGER = LogManager.getLogger(OrganizationController.class);
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public Response<List<Organization>> getAll() {
+        String log = "GET /organizations";
+        LOGGER.info(log);
         List<Organization> list = organizationService.getAll();
 
         Response<List<Organization>> response = new Response<>();
@@ -39,6 +44,8 @@ public class OrganizationController {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Response<Organization> get(@PathVariable int id) throws BaseException {
+        String log = "GET /organizations/" + id;
+        LOGGER.info(log);
         Organization organization = organizationService.get(id);
 
         Response<Organization> response = new Response<>();
@@ -50,6 +57,8 @@ public class OrganizationController {
     @Secured("ROLE_ADMIN")
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public Response<Organization> add(@RequestBody Organization organization) throws BaseException {
+        String log = "POST /organizations - Organization Name:" + organization.getName();
+        LOGGER.info(log);
         validateName(organization.getName());
         Organization organizationFromService = organizationService.add(organization);
 
--- src/main/java/com/monitise/controllers/TeamController.java
+++ src/main/java/com/monitise/controllers/TeamController.java
@@ -6,6 +6,8 @@ import com.monitise.models.Response;
 import com.monitise.models.Team;
 import com.monitise.services.OrganizationService;
 import com.monitise.services.TeamService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,10 +27,13 @@ public class TeamController {
     private SecurityHelper securityHelper;
     @Autowired
     private OrganizationService organizationService;
+    private static final Logger LOGGER = LogManager.getLogger(TeamController.class);
 
     @Secured("ROLE_ADMIN")
     @RequestMapping(value = "/teams/", method = RequestMethod.GET)
     public Response<List<Team>> getAll() {
+        String log = "GET /teams";
+        LOGGER.info(log);
         List<Team> list = teamService.getAll();
 
         Response<List<Team>> response = new Response<>();
@@ -40,6 +45,8 @@ public class TeamController {
     @Secured("ROLE_MANAGER")
     @RequestMapping(value = "/organizations/{organizationId}/teams/", method = RequestMethod.GET)
     public Response<List<Team>> getTeamListByOrganizationId(@PathVariable int organizationId) throws BaseException {
+        String log = "GET /organizations/" + organizationId + "/teams";
+        LOGGER.info(log);
         securityHelper.checkUserOrganizationAuthorization(organizationId);
         List<Team> list = teamService.getListFilterByOrganizationId(organizationId);
 
@@ -52,6 +59,8 @@ public class TeamController {
     @Secured("ROLE_MANAGER")
     @RequestMapping(value = "/organizations/{organizationId}/teams/{teamId}", method = RequestMethod.GET)
     public Response<Team> getTeamByOrganizationId(@PathVariable int organizationId, @PathVariable int teamId) throws BaseException {
+        String log = "GET /organizations/" + organizationId + "/teams/" + teamId;
+        LOGGER.info(log);
         securityHelper.checkUserOrganizationAuthorization(organizationId);
         Team team = teamService.get(teamId);
 
@@ -64,6 +73,8 @@ public class TeamController {
     @Secured("ROLE_MANAGER")
     @RequestMapping(value = "/organizations/{organizationId}/teams/", method = RequestMethod.POST)
     public Response<Team> addTeam(@PathVariable int organizationId, @RequestBody Team team) throws BaseException {
+        String log = "POST /organizations/" + organizationId + "/teams - Team Name: " + team.getName();
+        LOGGER.info(log);
         securityHelper.checkUserOrganizationAuthorization(organizationId);
         team.setOrganization(organizationService.get(organizationId));
         Team teamFromService = teamService.add(team);
