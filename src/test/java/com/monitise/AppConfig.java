6dabcc84166e36778f84cef4ecf7f3fb419545f5
Deniz Parmaksız <denizparmaksiz@gmail.com>
2016-08-15 11:28:28 +0300

    Configured tests to use embedded in-memory database H2, closing issue #19.

--- src/test/java/com/monitise/AppConfig.java
+++ src/test/java/com/monitise/AppConfig.java
@@ -1,12 +1,27 @@
 package com.monitise;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
 
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan
 public class AppConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("PerformhanceTest")
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .build();
+    }
 
 }
